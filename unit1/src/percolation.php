38e20a3c49307b8616443aa6452ecbd758409f1b
konservs <a@konservs.com>
2016-10-21 03:01:25 +0300

    Added first unit examples.

---
+++ unit1/src/percolation.php
@@ -0,0 +1,45 @@
+<?php
+class Percolation {
+	private $uf1;
+	private $uf2;
+	private $n;
+	private $grid;
+	/**
+	 * Create n-by-n grid, with all sites blocked
+	 */
+	public function __construct($n){
+		if (n <= 0) {
+			throw new IllegalArgumentException("N should be > 0");
+			}
+		$this->n = n;
+		$this->grid = [];
+		for ($i = 0; $i < $n; $i++) {
+			$this->grid[$i]=[];
+			for ($j = 0; $j < $n; $j++) {
+				$this->grid[$i][$j] = false;
+				}
+			}
+		$this->uf = new QuickUnion(n*n+2);
+		$this->uf2 = new QuickUnion(n*n+2);
+		}
+	/**
+	 * open site (row i, column j) if it is not open already
+	 */
+	public function open($i, $j) {
+		}
+	/**
+	 * is site (row i, column j) open?
+	 */
+	public function isOpen($i, $j) {
+		}
+	/**
+	 * is site (row i, column j) full?
+	 */
+	public function isFull($i, $j) {
+		}
+	/**
+	 * does the system percolate?
+	 */
+	public function percolates() {
+		}
+	}
---
+++ unit1/src/quickunion.php
@@ -0,0 +1,28 @@
+<?php
+
+public class QuickUnion{
+	private $id;
+	private $sz;
+	private $N;
+	/**
+	 * 
+	 */
+	public function __construct($N){
+		}
+	/**
+	 *
+	 */
+	public function root($i){
+		}
+	/**
+	 *
+	 */
+	public function union($p, $q){
+		}
+	/**
+	 *
+	 */
+	public function connected($p, $q){
+
+		}
+	}
